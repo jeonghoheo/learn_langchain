c9bbe91170479a3e9e8f13942000e0f925a70323
jeonghoheo <heojeongho1992@gmail.com>
2026-02-16 12:45:53 +0300

    feat: using a prompt template

--- hello_langchain.py
+++ hello_langchain.py
@@ -1,18 +1,19 @@
 from langchain_ollama import ChatOllama
-from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate
 
 # init Ollama model
 llm=ChatOllama(model="qwen2.5-coder")
 
-system_prompts = [
-    "You are a pirate. Say like a pirate.",
-    "You are a Shakespeare. Say it in an old-fashioned way.",
-    "You are a 5 years old kid. Say it in a childish way.",]
+# create prompt template
+template = ChatPromptTemplate.from_messages([
+    ("system", "You are {role}. answer this {style}."),
+    ("human", "{question}")])
 
-question = "What is the weather like today?"
+# apply prompt template
+message = template.format_messages(
+    role="Python expert",
+    style="in a concise way",
+    question="What is the difference between a list and a tuple in Python?"
+)
 
-for prompt in system_prompts:
-    print(f"[{prompt[:20]}...]")
-    messages = [SystemMessage(content=prompt), HumanMessage(content=question)]
-    response = llm.invoke(messages)
-    print(f"{response.content}\n")
+print(message)
